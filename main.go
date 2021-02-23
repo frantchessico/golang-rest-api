abe14ecba1123530971a832049b3f1eb9ad1082c
frantchessico <tchessico>
2021-02-23 15:58:56 +0200

    create server

--- main.go
+++ main.go
@@ -1,25 +1,37 @@
 package main
 
-import("fmt")
+import (
+	"fmt"
+	"net/http"
+	"log"
 
+	"github.com/gorilla/mux"
+	
+	
+)
 
 type tasks struct {
-	ID int  `json:ID`
-	Name string `json:Name`
+	ID      int    `json:ID`
+	Name    string `json:Name`
 	Content string `json:Content`
 }
 
 type allTasks []tasks
 
-var task = allTasks {
-    {
-		ID: 1,
-		Name: "Task one",
+var task = allTasks{
+	{
+		ID:      1,
+		Name:    "Task one",
 		Content: "Some Content",
 	},
 }
-func main() {
-    fmt.Println(task)
+
+func indexRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world")
 }
- 
- 
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", indexRoute)
+	log.Fatal(http.ListenAndServe(":3200", router))
+}
